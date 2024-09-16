fa9c547542ebbbcd42d4b5aad1f154fc33925114
Chen_Jia_Rou <supple0414@gmail.com>
2024-09-16 08:39:52 +0800

    Add files via upload

---
+++ wwwroot/MyJS.js
@@ -0,0 +1,66 @@
+﻿class UseAPI {
+
+    ListData(url,callback) {
+        $.ajax({
+            type: "Get",
+            url: url,
+            success: function (data) {
+                callback(data);
+            },
+            error: function (resp) {
+            }
+        });
+    }
+
+    GetData(url, callback) {
+        $.ajax({
+            type: "Get",
+            url: url,
+            success: function (data) {
+                callback(data);
+            },
+            error: function (resp) {
+            }
+        });
+    }
+
+    CreateData(url, jsonData, callback) {
+        $.ajax({
+            type: "Post",
+            url: url,
+            contentType: "application/json",
+            data: jsonData,
+            success: function (data) {
+                callback(data);
+            },
+            error: function (resp) {
+            }
+        });
+    }
+
+    UpdateData(url, jsonData, callback) {
+        $.ajax({
+            type: "Put",
+            url: url,
+            contentType: "application/json",
+            data: jsonData,
+            success: function (data) {
+                callback(data);
+            },
+            error: function (resp) {
+            }
+        });
+    }
+
+    DeleteData(url, callback) {
+        $.ajax({
+            type: "Delete",
+            url: url,
+            success: function (data) {
+                callback(data);
+            },
+            error: function (resp) {
+            }
+        });
+    }
+}
